4c992f9b7a9e2d4fe29f7fadf5fc66fa22dc849f
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2025-02-11 13:10:06 +0100

    Add compose paging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.paging.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
