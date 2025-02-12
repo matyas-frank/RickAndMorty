98f2d8cb539ef6e45e9e370c2602d02e4dd4bef6
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2025-02-12 16:20:22 +0100

    Create base viewmodel

---
+++ app/src/main/java/cz/frank/rickandmorty/utils/ui/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package cz.frank.rickandmorty.utils.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<State, Intent, Event> : ViewModel() {
+    private val _events = MutableSharedFlow<Event>()
+    val events = _events.asSharedFlow()
+    fun emitEvent(event: Event) { viewModelScope.launch { _events.emit(event) } }
+
+    abstract val state: StateFlow<State>
+    abstract fun onIntent(intent: Intent)
+}
