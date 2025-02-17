414e761201b2631a884ba8f667b885e3a7443696
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2025-02-17 19:06:09 +0100

    Create base UseCase for suspend work with result

--- app/src/main/java/cz/frank/rickandmorty/utils/BaseUseCase.kt
+++ app/src/main/java/cz/frank/rickandmorty/utils/BaseUseCase.kt
@@ -1,6 +1,7 @@
 package cz.frank.rickandmorty.utils
 
 import kotlinx.coroutines.flow.Flow
+import kotlin.Result
 
 
 interface UseCaseFlow<Result> {
@@ -9,4 +10,8 @@ interface UseCaseFlow<Result> {
 
 interface UseCaseFlowWithParams<Params, Result> {
     operator fun invoke(params: Params): Flow<Result>
+}
+
+interface UseCaseSuspendWithParams<Params, Res : Any> {
+    suspend operator fun invoke(params: Params): Result<Res>
 }
