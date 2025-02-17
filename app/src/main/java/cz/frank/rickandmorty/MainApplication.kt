c451d0a9bdd535b4fc17e981055056ccdbe417b3
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2025-02-17 13:45:44 +0100

    Add android context to koin

--- app/src/main/java/cz/frank/rickandmorty/MainApplication.kt
+++ app/src/main/java/cz/frank/rickandmorty/MainApplication.kt
@@ -2,6 +2,7 @@ package cz.frank.rickandmorty
 
 import android.app.Application
 import cz.frank.rickandmorty.di.rootModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
 class MainApplication : Application() {
@@ -9,6 +10,7 @@ class MainApplication : Application() {
         super.onCreate()
         startKoin {
             modules(rootModule)
+            androidContext(this@MainApplication)
         }
     }
 }
